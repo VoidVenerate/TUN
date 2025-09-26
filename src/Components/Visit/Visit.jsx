16ff7dba120655c0c59a2a299d06dee901d09766
Dada Emmanuel <dadaemmanuel751@gmail.com>
2025-09-26 21:39:25 +0100

    pol

--- src/Components/Visit/Visit.jsx
+++ src/Components/Visit/Visit.jsx
@@ -1,23 +1,22 @@
 import React from 'react'
 import vl from '../../assets/vl.png'
 import './Visit.css'
-import { NavLink, useLocation } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 const Visit = () => {
   return (
     <div className='visit-container'>
-        <button>
-          <NavLink
-            to="/lagvisit"
-            className={({ isActive }) => (isActive ? 'navbar-link active' : 'navbar-link')}
-            onClick={() => setMenuOpen(false)}
-          >
-            <img src={vl} />
-            <div className="caption">
-              <p>Discover the Vibrant Heartbeat of Lagos! From stunning Beaches and cultural landmarks to the hottest nightlife spots, find your next adventure in the city that never sleeps</p>
-            </div>
-          </NavLink>    
-        </button>
+      <NavLink
+        to="/lagvisit"
+        className={({ isActive }) => (isActive ? 'navbar-link active' : 'navbar-link')}
+      >
+        <img src={vl} alt="Visit Lagos" />
+        <div className="caption">
+          <p>
+            Discover the Vibrant Heartbeat of Lagos! From stunning Beaches and cultural landmarks to the hottest nightlife spots, find your next adventure in the city that never sleeps
+          </p>
+        </div>
+      </NavLink>    
     </div>
   )
 }
